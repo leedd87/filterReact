e7899d9aac0a566d41071b76d584318e40f6dae9
leedd87 <leedav87@gmail.com>
2022-06-09 17:57:51 -0300

    single filter

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card({ image, key, city }) {
+	return (
+		<div>
+			<img src={image} style={{ width: "350px" }} alt="Foto" />
+			<h2>{city}</h2>
+		</div>
+	);
+}
+export default Card;
